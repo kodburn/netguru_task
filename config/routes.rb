a78534817f3ef612e6a99dd7287e2e71f74a3fb6
kodburn <kodburn@gmail.com>
2017-01-22 00:13:38 +0100

    All controllers tests pass

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :students do
+  resources  :teachers
+  resources  :students do
     get :subjects
   end
-  resources :teachers
+  get "reports/subjects"
+  get "visitors/index"
 end
